94033319c16027861ab4a89cf3b4baaaf8e9e132
Matt Sykes <mtsykes1@sheffield.ac.uk>
2018-09-03 23:45:57 +0100

    Created struct for Set model.

---
+++ Set/Set.swift
@@ -0,0 +1,13 @@
+//
+//  Set.swift
+//  Set
+//
+//  Created by Matthew Sykes on 03/09/2018.
+//  Copyright © 2018 mattsykes. All rights reserved.
+//
+
+import Foundation
+
+struct Set {
+    // TODO: Enter code for card game of set (model).
+}
